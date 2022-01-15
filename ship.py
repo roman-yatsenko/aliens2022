9f11f1c3d9ad21691bc63b30214ea00691ef2503
roman.yatsenko <roman.yatsenko@hneu.net>
2022-01-15 18:24:52 +0200

    Add left movement

--- ship.py
+++ ship.py
@@ -14,13 +14,16 @@ class Ship:
         # Каждый новый корабль появляется у нижнего края экрана
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # Флаг перемещения
+        # Флаги перемещения
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         """"Обновляет позицию корабля с учетом флага."""
         if self.moving_right:
             self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
