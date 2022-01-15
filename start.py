38f9ec563757ff4d9211905b8dd6cfbbb3f54e76
roman.yatsenko <roman.yatsenko@hneu.net>
2022-01-15 17:39:34 +0200

    Add ship blitting

--- start.py
+++ start.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -17,6 +18,8 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
@@ -27,7 +30,8 @@ class AlienInvasion:
 
             # При каждом проходе цикла перерисовывается экран.
             self.screen.fill(self.settings.bg_color)
-
+            self.ship.blitme()
+            
             # Отображение последнего прорисованного экрана
             pygame.display.flip()
 
