83d68874301576fdeec36fdbba2e97a43cbd61b6
roman.yatsenko <roman.yatsenko@hneu.net>
2022-01-29 16:46:58 +0200

    Add alien row

--- start.py
+++ start.py
@@ -81,9 +81,19 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Создание флота вторжения."""
-        # Создание пришельца
+        # Вычисление количества пришельцев в ряду
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Создание первого ряда пришельцев
+        for alien_number in range(number_aliens_x):
+            # Создание пришельца
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
