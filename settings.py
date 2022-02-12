6dfdb7608ed2e93ff1e0068b1f12ac6ccd4e1bb2
roman.yatsenko <roman.yatsenko@hneu.net>
2022-02-12 17:10:47 +0200

    Refactor settings with speed

--- settings.py
+++ settings.py
@@ -9,18 +9,34 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Настройки корабля
-        self.ship_speed = 1.5
         self.ship_limit = 3
         
         # Параметры снаряда
-        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
         
         # Настройки пришельцев
-        self.alien_speed = 1.0
-        self.fleet_drop_speed = 100
+        self.fleet_drop_speed = 10
+
+        # Темп ускорения игры
+        self.speedup_scale = 1.1
+
+        self.initialyze_dynamic_settings()
+
+    def initialyze_dynamic_settings(self):
+        """Инициализирует настройки, изменяющиеся в ходе игры"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3.0
+        self.alien_speed_factor = 1.0
+        
         self.fleet_direction = 1
         # 1 - вправо, -1 - влево
+        
+    def increase_speed(self):
+        """Увеличивает настройки скорости"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+        
