c52c8aaa34f9d99d3c345b336153636eebfe51bf
roman.yatsenko <roman.yatsenko@hneu.net>
2022-02-12 16:23:38 +0200

    Add Button class

--- game_stats.py
+++ game_stats.py
@@ -5,7 +5,9 @@ class GameStats():
         """Инициализирует статистику."""
         self.settings = ai_game.settings
         self.reset_stats()
-        self.game_active = True
+
+        # Игра запускается в неактивном состоянии
+        self.game_active = False
 
     def reset_stats(self):
         """Сбрасывает статистику"""
--- settings.py
+++ settings.py
@@ -21,6 +21,6 @@ class Settings:
         
         # Настройки пришельцев
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 100
+        self.fleet_drop_speed = 10
         self.fleet_direction = 1
         # 1 - вправо, -1 - влево
