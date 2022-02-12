09b31c75a6010d545b9328f9548344499f41eee2
roman.yatsenko <roman.yatsenko@hneu.net>
2022-02-12 16:55:03 +0200

    Fix Play button bug

--- start.py
+++ start.py
@@ -62,7 +62,8 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Запускает новую игру при нажатии кнопки Play"""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
             # Сброс игровой статистики
             self.stats.reset_stats()
             self.stats.game_active = True
